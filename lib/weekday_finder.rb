5143774ad5c70ed3e909dd11396f1fbc8e0add59
Axel Straube & Stephen Ajayi <student@epicodus.com>
2015-04-21 16:10:48 -0700

    add functionality to determine a date's day of the week

--- lib/weekday_finder.rb
+++ lib/weekday_finder.rb
@@ -3,6 +3,18 @@ class Time
     date = self
     if date.sunday?
       "Sunday"
+    elsif date.monday?
+      "Monday"
+    elsif date.tuesday?
+      "Tuesday"
+    elsif date.wednesday?
+      "Wednesday"
+    elsif date.thursday?
+      "Thursday"
+    elsif date.friday?
+      "Friday"
+    else date.saturday?
+      "Saturday"  
     end
   end
 end
